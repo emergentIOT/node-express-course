bf01705bc1a836e24faf71f15fdd89995c4c523c
singh <aman24january@gmail.com>
2021-06-30 22:09:35 +1000

    Added routes

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 
+const mockData = [{name:"jack"}, {name:"jackson"}]
 
-app.listen(8000, function(){
-    console.log("Server is running");
+app.listen(8000, () => {console.log("Nodejs server started")});
+
+app.get("/users", function(req, res) {
+    res.json({
+        success: true,
+        message: 'Successfully got users!',
+        users: mockData
+    })
 })
