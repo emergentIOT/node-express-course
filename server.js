39a25181ab11ab32d96d03ac4e34ebaf7c313a83
singh <aman24january@gmail.com>
2021-06-30 22:19:26 +1000

    Added new endpoint

--- server.js
+++ server.js
@@ -6,9 +6,20 @@ const mockData = [{name:"jack"}, {name:"jackson"}]
 app.listen(8000, () => {console.log("Nodejs server started")});
 
 app.get("/users", function(req, res) {
+    
     res.json({
         success: true,
         message: 'Successfully got users!',
-        users: mockData
+        users: mockData,
+    })
+    console.log(`Data recived ${mockData}`);
+})
+
+app.get("/user/:id", function(req, res) {
+    
+    res.json({
+        success: true,
+        message: 'User found',
+        user:req.params.id
     })
 })
